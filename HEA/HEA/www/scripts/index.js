956bf340d5309478ed8e2aeb2bffd95dc61c0c7d
quentin pionnier <quentin.pionnier@gmail.com>
2019-11-28 00:27:44 +0900

    update of the map

--- HEA/HEA/www/scripts/index.js
+++ HEA/HEA/www/scripts/index.js
@@ -13,36 +13,87 @@
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        //////////////////////  Geolocation   //////////////////////
-        /*
-        var onSuccess = function (position) {
-            var element = document.getElementById('geolocation');
-            element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
-                'Longitude: ' + position.coords.longitude + '<br />' +
-                'Altitude: ' + position.coords.altitude + '<br />' +
-                'Heading: ' + position.coords.heading + '<br />' +
-                'Speed: ' + position.coords.speed + '<br />' +
-                'Timestamp: ' + position.timestamp + '<br />' +
-                '<hr />' + element.innerHTML;
+        /**
+  * Create google maps Map instance.
+  * @param {number} lat
+  * @param {number} lng
+  * @return {Object}
+  */
+        const createMap = ({ lat, lng }) => {
+            return new google.maps.Map(document.getElementById('map'), {
+                center: { lat, lng },
+                zoom: 15
+            });
         };
 
-        // onError Callback receives a PositionError object
-        //
-        function onError(error) {
-            alert('code: ' + error.code + '\n' +
-                'message: ' + error.message + '\n');
+        /**
+         * Create google maps Marker instance.
+         * @param {Object} map
+         * @param {Object} position
+         * @return {Object}
+         */
+        const createMarker = ({ map, position }) => {
+            return new google.maps.Marker({ map, position });
+        };
+
+        /**
+         * Track the user location.
+         * @param {Object} onSuccess
+         * @param {Object} [onError]
+         * @return {number}
+         */
+        const trackLocation = ({ onSuccess, onError = () => { } }) => {
+            if ('geolocation' in navigator === false) {
+                return onError(new Error('Geolocation is not supported by your browser.'));
+            }
+
+            return navigator.geolocation.watchPosition(onSuccess, onError, {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            });
+        };
+
+        /**
+         * Get position error message from the given error code.
+         * @param {number} code
+         * @return {String}
+         */
+        const getPositionErrorMessage = code => {
+            switch (code) {
+                case 1:
+                    return 'Permission denied.';
+                case 2:
+                    return 'Position unavailable.';
+                case 3:
+                    return 'Timeout reached.';
+            }
         }
 
-        var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000, enableHighAccuracy: true });
+        /**
+         * Initialize the application.
+         * Automatically called by the google maps API once it's loaded.
         */
-        
+        function init() {
+            const initialPosition = { lat: 59.32, lng: 17.84 };
+            const map = createMap(initialPosition);
+            const marker = createMarker({ map, position: initialPosition });
+            const $info = document.getElementById('info');
+
+            let watchId = trackLocation({
+                onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
+                    marker.setPosition({ lat, lng });
+                    map.panTo({ lat, lng });
+                    $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
+                    $info.classList.remove('error');
+                },
+                onError: err => {
+                    console.log($info);
+                    $info.textContent = `Error: ${err.message || getPositionErrorMessage(err.code)}`;
+                    $info.classList.add('error');
+                }
+            });
+        }
     };
 
     function onPause() {
