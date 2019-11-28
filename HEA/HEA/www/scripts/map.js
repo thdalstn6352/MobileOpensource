b22ca2582e6d3d8ceb0d684c367dbaf60d6aeca8
quentin pionnier <quentin.pionnier@gmail.com>
2019-11-28 15:29:58 +0900

    moving js in a specific file

---
+++ HEA/HEA/www/scripts/map.js
@@ -0,0 +1,28 @@
+﻿
+    // On vérifie si le navigateur supporte la géolocalisation
+    if (navigator.geolocation) {
+
+        function hasPosition(position) {
+            // Instanciation
+            var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+
+            // Ajustage des paramètres
+            myOptions = {
+                zoom: 15,
+                center: point,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+
+            // Envoi de la carte dans la div
+            mapDiv = document.getElementById("map"),
+            map = new google.maps.Map(mapDiv, myOptions),
+
+            marker = new google.maps.Marker({
+                position: point,
+                map: map,
+                // Texte du point
+                title: "You are here!"
+            });
+        }
+        navigator.geolocation.getCurrentPosition(hasPosition);
+    }
