ea0e5c131534793787119bc8b2704c2b5e9d2ab6
Min-soo Song <minsoo96@assam.dankook.ac.kr>
2019-12-01 15:00:43 +0900

    Merge Login&SignUp&Index

--- HEA/HEA/www/scripts/index.js
+++ HEA/HEA/www/scripts/index.js
@@ -11,38 +11,7 @@
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        //////////////////////  Geolocation   //////////////////////
-        /*
-        var onSuccess = function (position) {
-            var element = document.getElementById('geolocation');
-            element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
-                'Longitude: ' + position.coords.longitude + '<br />' +
-                'Altitude: ' + position.coords.altitude + '<br />' +
-                'Heading: ' + position.coords.heading + '<br />' +
-                'Speed: ' + position.coords.speed + '<br />' +
-                'Timestamp: ' + position.timestamp + '<br />' +
-                '<hr />' + element.innerHTML;
-        };
-
-        // onError Callback receives a PositionError object
-        //
-        function onError(error) {
-            alert('code: ' + error.code + '\n' +
-                'message: ' + error.message + '\n');
-        }
-
-        var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000, enableHighAccuracy: true });
-        */
-        
+        document.addEventListener('resume', onResume.bind(this), false );
     };
 
     function onPause() {
@@ -52,4 +21,4 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
